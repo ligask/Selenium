3bec8e96ee9efee6e33ee3bcbec02a34adc0cebf
ligask <kubaligas@gmail.com>
2022-05-17 09:52:47 +0200

    Hotel name select change to xpath

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class HotelSearch {
 
@@ -16,13 +17,16 @@ public class HotelSearch {
     public void searchHotel() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.booking.com");
 
         driver.findElement(By.id("onetrust-accept-btn-handler")).click();
         driver.findElement(By.id("ss")).click();
         driver.findElement(By.id("ss")).sendKeys("London");
-        driver.findElement(By.id("ss")).sendKeys(Keys.RETURN);
+        //driver.findElement(By.id("ss")).sendKeys(Keys.RETURN);
+        driver.findElement(By.xpath("//span[@class='search_hl_name' and text()='London']")).click();
+
 
 
 
