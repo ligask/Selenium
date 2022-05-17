30ee2b6d7878c9fe616132e58d9e1c53b4a61f76
ligask <kubaligas@gmail.com>
2022-05-17 21:54:30 +0200

    Set check-in and check-out dates

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,6 +10,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class HotelSearch {
@@ -26,6 +29,63 @@ public class HotelSearch {
         driver.findElement(By.id("ss")).sendKeys("London");
         //driver.findElement(By.id("ss")).sendKeys(Keys.RETURN);
         driver.findElement(By.xpath("//span[@class='search_hl_name' and text()='London']")).click();
+
+        String checkInMonth = "marzec 2023";
+        String checkOutMonth = "marzec 2023";
+        String checkInDay = "23";
+        String checkOutDay = "37";
+
+
+        while(true) {
+            String month = driver.findElement(By.xpath("(//div[@class='bui-calendar__month'])[1]")).getText();
+
+            if(month.equals(checkInMonth)) {
+                break;
+            } else {
+                driver.findElement(By.xpath("//div[@data-bui-ref='calendar-next']")).click();
+                break;
+            }
+        }
+
+        List<WebElement> inMonth = driver.findElements(By.xpath("(//tbody)[1]//tr//td//span"));
+
+        for(WebElement element:inMonth) {
+            String day = element.getText();
+
+            if(day.equals(checkInDay)) {
+                element.click();
+                break;
+            }
+            else {
+                System.out.println("Invalid check-in date");
+            }
+        }
+
+        while(true) {
+            String month = driver.findElement(By.xpath("(//div[@class='bui-calendar__month'])[1]")).getText();
+
+            if(month.equals(checkOutMonth)) {
+                break;
+            } else {
+                driver.findElement(By.xpath("//div[@data-bui-ref='calendar-next']")).click();
+            }
+        }
+
+        List<WebElement> outMonth = driver.findElements(By.xpath("(//tbody)[1]//tr//td//span"));
+
+        for(WebElement element:outMonth) {
+            String day = element.getText();
+
+            if(day.equals(checkOutDay)) {
+                element.click();
+                break;
+            }
+            else {
+                System.out.println("Invalid check-out date");
+                break;
+            }
+        }
+
 
 
 
